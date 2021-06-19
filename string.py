9c9931c9a4f8d8dde1365418f440cc5aadef3048
hcarvalho <hcarvalho@compugraf.com.br>
2021-06-19 11:48:59 -0300

    curso basico

---
+++ string.py
@@ -0,0 +1,20 @@
+nome = 'Adriano Leita da Silva'
+
+nome[0]
+print(nome)
+print(nome[0])
+print(nome[1])
+print(nome[2])
+print(nome[-1])
+print(nome[2:6])
+print(nome[-5:])
+
+frase = 'Python é demais'
+
+print('thon' in frase)
+
+print('thon' not in frase)
+
+
+print(frase.lower())
+print(frase.upper())
